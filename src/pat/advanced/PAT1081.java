c9903c61455d8320442ec9316a9561c7aeabd603
zhangc47 <1228073191@qq.com>
2021-07-13 13:19:30 +0800

    1080

---
+++ src/pat/advanced/PAT1081.java
@@ -0,0 +1,71 @@
+package pat.advanced;
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ /**
+ * Sample Input 1:
+ * 5
+ * 2/5 4/15 1/30 -2/60 8/3
+ * Sample Output 1:
+ * 3 1/3
+ *
+ * Sample Output 2:
+ * 2
+ * 4/3 2/3
+ * output:
+ * 2
+ *
+ *
+ * Sample Output 3:
+ * 3
+ * 1/3 -1/6 1/8
+ * output:
+ * 7/24
+ *
+ *
+ */
+
+public class PAT1081 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String n = reader.readLine();
+        String[] s1 = reader.readLine().split(" ");
+        String[] s2 = s1[0].split("/");
+        int a = Integer.parseInt(s2[0]);
+        int b = Integer.parseInt(s2[1]);
+        int sum = 0;
+        for (int i = 1; i < Integer.parseInt(n); i++) {
+            String[] s = s1[i].split("/");
+            int atemp = Integer.parseInt(s[0]);
+            int btemp = Integer.parseInt(s[1]);
+            a = a * btemp + atemp * b;
+            b = b * btemp;
+            int c = GCD(a, b);
+            a = a / c;
+            b = b / c;
+        }
+        if (a % b == 0) {
+            System.out.print(a / b);
+        } else {
+            if (Math.abs(a) > Math.abs(b)) {
+                if (a > 0) {
+                    System.out.printf("%d %d/%d", a / b, a - a / b * b, b);
+                } else {
+                    a = -a;
+                    System.out.printf("-%d %d/%d", a / b, a - a / b * b, b);
+                }
+            } else {
+                System.out.printf("%d/%d", a - a / b * b, b);
+            }
+        }
+    }
+
+    private static int GCD(int m, int n) {
+        return n == 0 ? m : GCD(n, m % n);
+    }
+}
---
+++ src/pat/advanced/RationalSum_1081.java
@@ -0,0 +1,66 @@
+package pat.advanced;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Sample Input 1:
+ * 5
+ * 2/5 4/15 1/30 -2/60 8/3
+ * Sample Output 1:
+ * 3 1/3
+ *
+ * Sample Output 2:
+ * 2
+ * 4/3 2/3
+ * output:
+ * 2
+ *
+ *
+ * Sample Output 3:
+ * 3
+ * 1/3 -1/6 1/8
+ * output:
+ * 7/24
+ *
+ *
+ */
+
+/**
+ * 0的情况
+ *
+ * 找到分母的最大公约数GCD，Greatest Common Divisor(GCD)
+ * GCD(a,b) is the smallest positive linear combination of a and b
+ *
+ */
+
+public class RationalSum_1081 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String inputStr = br.readLine();
+        String[] v1 = inputStr.split(" ");
+        Long result = 0l;
+        // numerator/denominator
+        for (int i = 0; i < N; i++) {
+            String[] v2 = v1[i].split("/");
+            Long numerator = Long.parseLong(v2[0]);
+            Long denominator = Long.parseLong(v2[1]);
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
