6ce35c398ec28b2f721487f291c1c6e3fab4c21c
zhangc <1228073191@qq.com>
2021-11-10 14:11:48 +0800

    ti :wq
    :wq
    i
    :wq

--- src/pat/advanced/rational/GCD.java
+++ src/pat/advanced/rational/GCD.java
@@ -5,10 +5,52 @@ package pat.advanced.rational;
  */
 public class GCD {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 1;
+        int a = 28;
+        int b = 12;
 
-        solution1(a, b);
+        // 暴力法
+        int s1 = solution1(a, b);
+
+        // 递归法
+        int s2 = solution2(47, 100);
+
+        // 欧几里得法,辗转相除法
+        int s3 = solution3(a, b);
+    }
+
+    /**
+     * 欧几里得法,辗转相除法
+     */
+    private static int solution3(int m, int n) {
+        /*if (m < n) {
+            int temp = m;
+            m = n;
+            n = temp;
+        }*/
+        while (n > 0) {
+            int temp = m % n;
+            m = n;
+            n = temp;
+        }
+        return m;
+    }
+
+    /**
+     * 递归的方法
+     */
+    private static int solution2(int m, int n) {
+        // 保证m > n
+        if (m < n) {
+            int temp = m;
+            m = n;
+            n = temp;
+        }
+        if (m % n == 0) {
+            return n;
+        } else {
+            return solution2(n, m % n);
+        }
+
 
     }
 
@@ -17,7 +59,7 @@ public class GCD {
      * 从两个数之间找最小的数，然后用这个数往下减，若是两个数都能够被整除，那个数就是最大公约数
      */
     private static int solution1(int m, int n) {
-        int temp = m > n ? n : m;
+        int temp = Math.min(m, n);
         for (int i = temp; i > 0; i--) {
             if (m % i == 0 && n % i == 0) {
                 return i;
--- src/pat/advanced/rational/LeastCommonMultiple_LCM.java
+++ src/pat/advanced/rational/LeastCommonMultiple_LCM.java
@@ -1,2 +1,31 @@
-package pat.advanced.rational;public class LeastCommonMultiple_LCM {
+package pat.advanced.rational;
+
+/**
+ * https://www.cnblogs.com/liuzhen1995/p/6423600.html
+ * lcm(m,n) = (m*n) / gcd(m,n)
+ */
+public class LeastCommonMultiple_LCM {
+    //使用欧几里得算法求解数m和数n最大公约数
+    public int getGcd(int m,int n){
+        while(n > 0){
+            int temp = m % n;
+            m = n;
+            n = temp;
+        }
+        return m;
+    }
+
+    //求解数m和n和最小公倍数
+    public int getLcm(int m,int n){
+        int gcd = getGcd(m,n);
+        int result = m * n / gcd;
+        return result;
+    }
+
+    public static void main(String[] args){
+        LeastCommonMultiple_LCM test = new LeastCommonMultiple_LCM();
+        System.out.println("60和12的最大公约数："+test.getGcd(60, 12));
+        System.out.println("60和12的最小公倍数："+test.getLcm(60, 12));
+    }
+
 }
