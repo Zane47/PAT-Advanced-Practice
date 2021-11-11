6210c870869a87c11032b8a1cb58fab52367faea
zhangc <1228073191@qq.com>
2021-11-11 09:47:53 +0800

    1111

--- src/pat/advanced/dp/MaximumSubsequenceSum_1007.java
+++ src/pat/advanced/dp/MaximumSubsequenceSum_1007.java
@@ -11,6 +11,8 @@ import java.util.Scanner;
  * -10 1 2 3 4 -5 -23 3 7 -21
  * <p>
  * 10 1 4
+ *
+ * 测试点7超时, 测试点4错误
  */
 public class MaximumSubsequenceSum_1007 {
     public static void main(String[] args) {
@@ -24,13 +26,52 @@ public class MaximumSubsequenceSum_1007 {
         }
 
         // 2.dp
-        // dp：以这个位置为结尾的数字的最大子序列的和
-        // 因为这里要记录start和end，所以就不用dp数组了
-        // 使用temp来记录i-1时刻的值，再lc53中我们使用了一整个数组，
-        // 其实是不需要的，直接可以用一个数字来代表dp[i-1]即可，因为我们只需要max
+        /**
+         * 1.dp：以这个位置为结尾的数字的最大子序列的和
+         * 因为这里要记录start和end，所以就不用dp数组了
+         * 使用temp来记录i-1时刻的值，再lc53中我们使用了一整个数组，
+         * 其实是不需要的，直接可以用一个数字来代表dp[i-1]即可，因为我们只需要max
+         *
+         * 2.记录max开始的位置start和结束end的位置，
+         *
+         * 3.与lc53不同的地方：
+         * If all the K numbers are negative,
+         * then its maximum sum is defined to be 0,
+         * and you are supposed to output the first and the last numbers of the whole sequence.
+         */
+        boolean allNegativeFlag = true;
+        for (int num : nums) {
+            if (num > 0) {
+                allNegativeFlag = false;
+                break;
+            }
+        }
+        if (allNegativeFlag) {
+            System.out.printf("%d %d %d", 0, nums[0], nums[nums.length - 1]);
+        } else {
+            int maxSum = Integer.MIN_VALUE;
+            // dp[i-1]
+            int subSum = 0;
+            // endIndex: 我们是知道的, 就是如果遍历的时候加入了nums[i], 那就是i
+            // startIndex: 我们需要记录上一次的,
+            // 如果纳入了dp[i-1]那就是dp[i-1]的起点, 如果没有纳入dp[i-1]那就是这个数字
+            int tempStartIndex = 0, startIndex = 0, endIndex = 0;
 
+            for (int i = 0; i < nums.length; i++) {
+                subSum = subSum + nums[i];
+                if (subSum < 0) {
+                    // 负数, 那么就+1
+                    subSum = 0;
+                    tempStartIndex = i + 1;
+                } else if (subSum > maxSum) {
+                    maxSum = subSum;
+                    startIndex = tempStartIndex;
+                    endIndex = i;
+                }
 
-
+            }
+            System.out.printf("%d %d %d", maxSum, nums[startIndex], nums[endIndex]);
+        }
 
     }
 
