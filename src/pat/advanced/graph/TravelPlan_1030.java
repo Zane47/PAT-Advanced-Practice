e61cd4c767a26ccdbe1d2c6b05d93eac6f048b8f
zhangc <1228073191@qq.com>
2021-11-19 10:57:12 +0800

    1

---
+++ src/pat/advanced/graph/TravelPlan_1030.java
@@ -0,0 +1,46 @@
+package pat.advanced.graph;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 单源最短路径
+ */
+public class TravelPlan_1030 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // number of cities
+        int N = sc.nextInt();
+        // # of highways
+        int M = sc.nextInt();
+        // start cities
+        int S = sc.nextInt();
+        // destination cities
+        int D = sc.nextInt();
+
+        int[][] distance = new int[N][N];
+        int[][] cost = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(distance[i], Integer.MAX_VALUE);
+        }
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(cost[i], Integer.MAX_VALUE);
+        }
+
+        for (int i = 0; i < M; i++) {
+            int city1 = sc.nextInt();
+            int city2 = sc.nextInt();
+            int d = sc.nextInt();
+            int c = sc.nextInt();
+            distance[city1][city2] = d;
+            distance[city2][city1] = d;
+            cost[city1][city2] = c;
+            cost[city2][city1] = c;
+        }
+        int a = 0;
+
+
+
+
+    }
+}
---
+++ src/pat/advanced/graph/traversal.java
@@ -0,0 +1,30 @@
+package pat.advanced.graph;
+
+/**
+ * 单源遍历
+ * start -> destination
+ */
+public class traversal {
+
+
+    public static void main(String[] args) {
+        int[][] graph = new int[][] {
+                {},
+                {},
+                {},
+                {}
+        };
+
+        // dfs();
+
+        // bfs();
+
+
+
+    }
+
+
+
+
+
+}
