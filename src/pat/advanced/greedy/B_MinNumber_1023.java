a3cc71b471ed8a170614e1c9377d2e0b2bdb7311
zhangc <1228073191@qq.com>
2021-11-25 13:54:43 +0800

    1

--- src/pat/advanced/greedy/B_MinNumber_1023.java
+++ src/pat/advanced/greedy/B_MinNumber_1023.java
@@ -1,6 +1,7 @@
 package pat.advanced.greedy;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -11,11 +12,29 @@ import java.io.InputStreamReader;
  *
  */
 public class B_MinNumber_1023 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int[] nums = new int[10];
+        for (int i = 0; i < 10; i++) {
+            nums[i] = Integer.parseInt(input[i]);
+        }
 
+        // 首位不能是0
+        for (int i = 1; i < 10; i++) {
+            if (nums[i] != 0) {
+                System.out.print(i);
+                nums[i]--;
+                break;
+            }
+        }
 
-
+        // 第二位开始
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < nums[i]; j++) {
+                System.out.print(i);
+            }
+        }
 
     }
 }
