95afd6289cef20c9a92ddf624213f6e9face75c4
zhangc47 <1228073191@qq.com>
2021-11-29 10:29:13 +0800

    1

--- src/pat/advanced/bfs/AcuteStroke_1091.java
+++ src/pat/advanced/bfs/AcuteStroke_1091.java
@@ -1,12 +1,33 @@
 package pat.advanced.bfs;
 
+import java.util.Scanner;
+
+/**
+ * 3d的图, 来求区域, -> bfs求区域
+ *
+ *
+ *
+ *
+ *
+ */
 public class AcuteStroke_1091 {
 
 
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // M and N are the sizes of each slice
+        // (i.e. pixels of a slice are in an M×N matrix,
+        // and the maximum resolution is 1286 by 128
+        int M = sc.nextInt();
+        int N = sc.nextInt();
 
+        // # of slices of a brain, <= 60
+        int L = sc.nextInt();
 
+        // T is the integer threshold
+        // (i.e. if the volume of a connected core is less than T, then that core must not be counted).
+        int T = sc.nextInt();
 
 
 
