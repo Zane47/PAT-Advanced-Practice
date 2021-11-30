07542c013537b7ae366eeaaca299d040dbacdf93
zhangc <1228073191@qq.com>
2021-11-30 13:41:17 +0800

    1

---
+++ src/pat/advanced/simulation/HelloWorld4U_1031.java
@@ -0,0 +1,20 @@
+package pat.advanced.simulation;
+
+/**
+ * N >=5的字母
+ * 保持原序
+ *
+ * n1: 左上到左下, 左边的一竖
+ * n2: 左下到右下, 下边的一横
+ * n3: 左下到右上, 右边的一竖
+ *
+ *
+ */
+public class HelloWorld4U_1031 {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
