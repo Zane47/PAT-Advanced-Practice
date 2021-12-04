1e992da443aa7581a38cc5a10908757378f5cdf9
zhangc47 <1228073191@qq.com>
2021-12-04 17:17:59 +0800

    111

--- src/pat/advanced/link/ReversingLinkedList_1074.java
+++ src/pat/advanced/link/ReversingLinkedList_1074.java
@@ -1,10 +1,18 @@
 package pat.advanced.link;
 
+import java.util.Scanner;
+
 /**todo:
  * 每k个元素反转链表
  */
 public class ReversingLinkedList_1074 {
+
+    private static Node[] nodes;
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        nodes = new Node[100010];
 
 
 
