96bbfabc0ed29c5eae96b7562612ecc7673fc136
zhangc47 <1228073191@qq.com>
2021-12-05 22:01:56 +0800

    1

--- src/pat/advanced/hash/FindCoins_1048.java
+++ src/pat/advanced/hash/FindCoins_1048.java
@@ -9,16 +9,17 @@ import java.util.Scanner;
  * Since she has as many as 10^5 coins with her,
  * tell her, for any given amount of money,
  * whether or not she can find two coins to pay for it.
- *
+ * <p>
+ * 注意点: 这里的hashtable不能只开500, 因为m-num[i]可能大于500
  */
 public class FindCoins_1048 {
     public static void main(String[] args) {
 
         // 测试点3, 4超时
-        // solution1();
+        solution1();
 
 
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         // the total number of coins, <=10^5
         int N = sc.nextInt();
         // the amount of money Eva has to pay, <=10^3
@@ -32,7 +33,7 @@ public class FindCoins_1048 {
         for (int i = 0; i < N;i++) {
             coins[i] = sc.nextInt();
             table[coins[i]]++;
-        }
+        }*/
     }
 
     /**
@@ -49,26 +50,33 @@ public class FindCoins_1048 {
         int[] coins = new int[N];
 
         // hashtable
-        int[] table = new int[501];
+        int[] table = new int[10001];
 
-        for (int i = 0; i < N;i++) {
+        for (int i = 0; i < N; i++) {
             coins[i] = sc.nextInt();
             table[coins[i]]++;
         }
 
         Arrays.sort(coins);
 
-        for (int coin : coins) {
-            table[coin]--;
+        for (int i : coins) {
+            /*table[coin]--;
             int rest = M - coin;
             if (rest >= 0 && table[rest] != 0) {
                 System.out.print(coin + " " + rest);
+                return;
+            }*/
+
+            if (table[i] >= 0 && table[M - i] >= 0) {
+                if ((M - i == i) && table[i] <= 1) {
+                    continue;
+                }
+                System.out.print(i + " " + (M - i));
                 return;
             }
 
         }
         System.out.print("No Solution");
-
 
 
     }
