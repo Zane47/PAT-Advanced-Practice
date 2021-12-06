7aa2b7557537d9f43e64b1bc52049d10ce95fb1f
Zane47 <1228073191@qq.com>
2021-12-06 09:14:07 +0800

    111

--- src/pat/advanced/greedy/ToFillOrNotToFill_1033.java
+++ src/pat/advanced/greedy/ToFillOrNotToFill_1033.java
@@ -1,10 +1,12 @@
 package pat.advanced.greedy;
 
+import java.util.Scanner;
+
 public class ToFillOrNotToFill_1033 {
 
     public static void main(String[] args) {
 
-
+        Scanner sc = new Scanner(System.in);
 
 
 
