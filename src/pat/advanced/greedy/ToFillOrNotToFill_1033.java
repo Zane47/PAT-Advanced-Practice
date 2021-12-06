ae7ca6009299e9d8c1420e8ae0c293559f41d7e3
Zane47 <1228073191@qq.com>
2021-12-06 09:28:56 +0800

    Update ToFillOrNotToFill_1033.java

--- src/pat/advanced/greedy/ToFillOrNotToFill_1033.java
+++ src/pat/advanced/greedy/ToFillOrNotToFill_1033.java
@@ -3,13 +3,24 @@ package pat.advanced.greedy;
 import java.util.Scanner;
 
 /**
- * 
+ *
  */
 public class ToFillOrNotToFill_1033 {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        // the maximum capacity of the tank, <=100
+        int cMax = sc.nextInt();
+        // D (<=30000), the distance between Hangzhou and the destination city
+        int D = sc.nextInt();
+        // the average distance per unit gas that the car can run;<=20
+        int dAvg = sc.nextInt();
+        // and N (≤ 500), the total number of gas stations
+        int N = sc.nextInt();
+
+        
+
 
 
 
