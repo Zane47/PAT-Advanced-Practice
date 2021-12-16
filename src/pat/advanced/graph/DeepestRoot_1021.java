5263995c157740b54cc5f532f7c5d5dd5930124c
Zane47 <1228073191@qq.com>
2021-12-16 14:59:24 +0800

    1

--- src/pat/advanced/graph/DeepestRoot_1021.java
+++ src/pat/advanced/graph/DeepestRoot_1021.java
@@ -2,16 +2,29 @@ package pat.advanced.graph;
 
 import java.util.Scanner;
 
-/**
+/**todo: 得用并查集
+ * find the root that results in a highest tree. Such a root is called the deepest root.
+ * 找最深的那个树
  *
+ * N个结点, N-1条边, 能否形成树, 如果可以选取使树最深的root
+ *
+ * 1.
+ *
+ * 2. 如果不是树,输出# of connected components in the graph.
  */
 public class DeepestRoot_1021 {
     public static void main(String[] args) {
-
+        // ------------------------ input+init ------------------------
         Scanner sc = new Scanner(System.in);
+        // # of nodes, <=10^4, [1,N]
+        int N = sc.nextInt();
+
+        // N-1 lines, each describes an edge by given the two adjacent nodes' numbers.
+        for (int i = 0; i < N - 1; i++) {
 
 
 
+        }
 
 
 
