243d8d2d4a7a4d446e3899764773d1156eafdbd3
Zane47 <1228073191@qq.com>
2021-12-16 11:17:36 +0800

    bfs

---
+++ src/pat/advanced/graph/DeepestRoot_1021.java
@@ -0,0 +1,19 @@
+package pat.advanced.graph;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class DeepestRoot_1021 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+
+    }
+}
