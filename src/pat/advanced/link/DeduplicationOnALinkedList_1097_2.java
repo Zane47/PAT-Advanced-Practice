76d32377046a464d0c511a266c46904df40c5221
zhangc47 <1228073191@qq.com>
2021-12-16 22:31:23 +0800

    1

--- src/pat/advanced/link/DeduplicationOnALinkedList_1097_2.java
+++ src/pat/advanced/link/DeduplicationOnALinkedList_1097_2.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**测试点1错误
- * 测试点3, 4超时
+/**
  * remove the nodes with duplicated absolute values of the keys
  * 绝对值一致
  * 00100 5
@@ -21,7 +20,7 @@ import java.util.Scanner;
  * 00000 -15 87654
  * 87654 15 -1
  */
-public class DeduplicationOnALinkedList_1097 {
+public class DeduplicationOnALinkedList_1097_2 {
 
     public static void main(String[] args) {
         // ------------------------ input, initial ------------------------
@@ -44,61 +43,17 @@ public class DeduplicationOnALinkedList_1097 {
             nodes[address].next = sc.nextInt();
         }
 
-        // key is an integer of which absolute value is no more than 10
-        // hashTable的方式记录, 如果abs>1的话就说明重复了
-        int[] hashTable = new int[10010];
 
         // ------------------------ do ------------------------
-        // 首先遍历一遍, 然后记录每一个key的出现次数
+        // 绝对值是否已经出现
+        boolean[] hasShow = new boolean[100010];
 
-        // 个数
-        int count = 0;
-        int cur = first;
-        while (cur != -1) {
-            hashTable[Math.abs(nodes[cur].key)]++;
-            count++;
-            cur = nodes[cur].next;
-        }
 
-        // 无效结点处理, head就不在link上
-        if (count == 0) {
-            return;
-        }
 
-        // 再次遍历
+
         List<Node> resultList = new ArrayList<>();
         List<Node> removedList = new ArrayList<>();
 
-        // 记录是否是第一次遍历到该数字的绝对值
-        boolean[] nums = new boolean[10010];
-
-        // 记录removedList中的key, 是否存在removedList中
-        List<Integer> removedNumList = new ArrayList<>();
-
-        cur = first;
-        while (cur != -1) {
-            Node node = nodes[cur];
-
-            // 序列中key仅出现了一次
-            if (hashTable[Math.abs(node.key)] == 1) {
-                resultList.add(node);
-            } else {
-                // 整个序列中不止一个key
-                // 如果是第一次出现, 就加入resultList
-                if (!nums[Math.abs(node.key)]) {
-                    resultList.add(node);
-                    nums[Math.abs(node.key)] = true;
-                } else {
-                    // 如果不是第一次出现, 判断是否要加入removedList
-                    if (!removedNumList.contains(node.key)) {
-                        removedList.add(node);
-                        removedNumList.add(node.key);
-                    }
-                }
-            }
-            // 下一个
-            cur = nodes[cur].next;
-        }
         // ------------------------ ouput ------------------------
         // output the resulting linked list first, then the removed list.
         // Each node occupies a line, and is printed in the same format as in the input.
